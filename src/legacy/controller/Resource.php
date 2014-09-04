4bc1b1728e7d02ac5ea780758c1408f8ef1a6a8a
@ <sleeping.cait.sith+gh@gmail.com>
2014-09-04 15:23:07 +0900

    add legacy controller\Resource, controller\Test.

---
+++ src/legacy/controller/Resource.php
@@ -0,0 +1,34 @@
+<?php
+
+if (false){
+
+	/**
+	 * atomita_codeigniter_controller_Resource
+	 *
+	 * @author atomita
+	 */
+	class atomita_codeigniter_controller_Resource extends MY_Controller
+	{
+		function _remap($method, $params = array()){}
+	}
+
+}
+else{
+	$ds = DIRECTORY_SEPARATOR;
+	$file = implode($ds, explode("{$ds}legacy{$ds}", __FILE__));
+
+	$definition = ltrim(file_get_contents($file), '<?php');
+
+	eval(str_replace(
+		array(
+			'namespae atomita\\codeigniter\\controller;',
+			'use \\MY_Controller;',
+			'class Resource',
+		),
+		array(
+			'',
+			'',
+			'class atomita_codeigniter_controller_Resource',
+		),
+		$definition));
+}
---
+++ src/legacy/controller/Test.php
@@ -0,0 +1,35 @@
+<?php
+
+if (false){
+
+	/**
+	* atomita_codeigniter_controller_Test
+	*
+	* @author atomita
+	*/
+	class atomita_codeigniter_controller_Test extends MY_Controller
+	{
+		function __construct(){}
+		function _output($output){}
+	}
+
+}
+else{
+	$ds = DIRECTORY_SEPARATOR;
+	$file = implode($ds, explode("{$ds}legacy{$ds}", __FILE__));
+
+	$definition = ltrim(file_get_contents($file), '<?php');
+
+	eval(str_replace(
+		array(
+			'namespae atomita\\codeigniter\\controller;',
+			'use \\MY_Controller;',
+			'class Test',
+		),
+		array(
+			'',
+			'',
+			'class atomita_codeigniter_controller_Test',
+		),
+		$definition));
+}
